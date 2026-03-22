0e7116332b3d76da70aa5e1800299dec3e194452
wise-fern <wise.fern2345@fastmail.com>
2026-03-22 09:54:53 -0400

    define constructor

--- libyunpa/src/Graphics/Sizable.cpp
+++ libyunpa/src/Graphics/Sizable.cpp
@@ -4,4 +4,6 @@ module libyunpa;
 
 namespace libyunpa {
 Sizable::~Sizable() = default;
-}
+
+Sizable::Sizable(const Options &options) : MinSize(options), MaxSize(options) {}
+} // namespace libyunpa
