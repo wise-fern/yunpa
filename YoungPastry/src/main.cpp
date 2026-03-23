f4fc59978d83bdfb5ae2716b666071110c8a3c34
wise-fern <wise.fern2345@fastmail.com>
2026-03-23 19:20:35 -0400

    test latest bits of libyunpa

--- YoungPastry/src/main.cpp
+++ YoungPastry/src/main.cpp
@@ -1,14 +1,22 @@
 import libyunpa;
 import YoungPastry;
 #include <iostream>
-#include <memory>
 
 int main() {
-  using namespace YoungPastry;
-  using libyunpa::Core;
-  Core::Init();
-  auto title{std::make_shared<Scenes::Title>()};
-  Core::SetNextScene(title);
-  Core::Run();
-  std::cout << "Hello world!\n";
+  libyunpa::Text::Options options{};
+  options.text = "Hello, World!";
+  options.color = libyunpa::Color::AQUA;
+  options.bgColor = libyunpa::Color::MAROON;
+  options.position = {31, 10};
+  auto test{libyunpa::Text{options}};
+  options.blink = true;
+  options.position.y = 11;
+  auto test2{libyunpa::Text{options}};
+  std::cout << "\x1b[?1049h";
+  test.draw();
+  test2.draw();
+  std::cout << '\n';
+  std::string input;
+  std::cin >> input;
+  std::cout << "\x1b[?1049l";
 }
