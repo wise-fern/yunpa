5f15a17124ce1b964cb16565935040477838b9d0
wise-fern <wise.fern2345@fastmail.com>
2026-03-26 19:43:23 -0400

    set title scene

--- YoungPastry/src/main.cpp
+++ YoungPastry/src/main.cpp
@@ -1,22 +1,12 @@
 import libyunpa;
 import YoungPastry;
-#include <iostream>
+#include <memory>
 
 int main() {
-  libyunpa::Text::Options options{};
-  options.text = "Hello, World!";
-  options.color = libyunpa::Color::AQUA;
-  options.bgColor = libyunpa::Color::MAROON;
-  options.position = {31, 10};
-  auto test{libyunpa::Text{options}};
-  options.blink = true;
-  options.position.y = 11;
-  auto test2{libyunpa::Text{options}};
-  std::cout << "\x1b[?1049h";
-  test.draw();
-  test2.draw();
-  std::cout << '\n';
-  std::string input;
-  std::cin >> input;
-  std::cout << "\x1b[?1049l";
+  using libyunpa::Core;
+  using namespace YoungPastry;
+  auto title{std::make_shared<Scenes::Title>()};
+  Core::Init();
+  Core::SetNextScene(title);
+  Core::Run();
 }
