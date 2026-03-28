f96631864cf949286109459305e97a92614a08fd
wise-fern <wise.fern2345@fastmail.com>
2026-03-28 10:40:35 -0400

    add _getch for non-windows

--- libyunpa/src/Engine/EventManager.cpp
+++ libyunpa/src/Engine/EventManager.cpp
@@ -36,6 +36,27 @@ int _kbhit() {
   ioctl(STDIN, FIONREAD, &bytesWaiting);
   return bytesWaiting;
 }
+
+int _getch() {
+  int buf = 0;
+  struct termios old = {0};
+  fflush(stdout);
+  if (tcgetattr(0, &old) < 0)
+    perror("tcsetattr()");
+  old.c_lflag &= ~ICANON;
+  old.c_lflag &= ~ECHO;
+  old.c_cc[VMIN] = 1;
+  old.c_cc[VTIME] = 0;
+  if (tcsetattr(0, TCSANOW, &old) < 0)
+    perror("tcsetattr ICANON");
+  if (read(0, &buf, 1) < 0)
+    perror("read()");
+  old.c_lflag |= ICANON;
+  old.c_lflag |= ECHO;
+  if (tcsetattr(0, TCSADRAIN, &old) < 0)
+    perror("tcsetattr ~ICANON");
+  return buf;
+}
 #endif
 
 namespace Grammar {
