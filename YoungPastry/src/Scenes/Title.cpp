8122053e756d6ebce9a9d2362b9c66a8947db2f8
wise-fern <wise.fern2345@fastmail.com>
2026-03-28 10:41:59 -0400

    add and draw title

--- YoungPastry/src/Scenes/Title.cpp
+++ YoungPastry/src/Scenes/Title.cpp
@@ -3,9 +3,17 @@ module;
 module YoungPastry;
 
 namespace YoungPastry::Scenes {
+Title::Title() {
+  _title.text("Young Pastry");
+  _title.centered(true);
+  _title.color(libyunpa::Color::RED);
+}
+
 void Title::update(const libyunpa::GameTime &gameTime) {}
 
-void Title::draw() const {}
+void Title::draw() const {
+  _title.draw();
+}
 
 void Title::handle_event(const libyunpa::Events::KeyEvent &event) {
   if (event.isKeyDown) {
--- YoungPastry/src/Scenes/Title.cppm
+++ YoungPastry/src/Scenes/Title.cppm
@@ -6,7 +6,10 @@ import libyunpa;
 namespace YoungPastry::Scenes {
 export class Title : public libyunpa::Scene {
 private:
+  libyunpa::Text _title;
+
 public:
+  Title();
   void update(const libyunpa::GameTime &gameTime) override;
   void draw() const override;
   void handle_event(const libyunpa::Events::KeyEvent &event) override;
