75bf4d5a0d81e83271362bc3b505fcd594ddf643
wise-fern <wise.fern2345@fastmail.com>
2026-03-30 16:37:04 -0400

    generate the initial title

--- YoungPastry/src/Scenes/Title.cpp
+++ YoungPastry/src/Scenes/Title.cpp
@@ -6,7 +6,18 @@ namespace YoungPastry::Scenes {
 void Title::update(const libyunpa::GameTime &gameTime) {}
 
 ftxui::Element Title::draw() const {
-  return ftxui::text("Young Pastry") | ftxui::center;
+  constexpr auto SPACER_HEIGHT{5};
+  auto youngpastry{ftxui::text("Young Pastry") | ftxui::center |
+                   ftxui::color(ftxui::Color::DeepPink3)};
+  auto in_{ftxui::text("in") | ftxui::center |
+           ftxui::color(ftxui::Color::DeepPink2)};
+  auto search{ftxui::text("The Search for a Magical Pet") | ftxui::center |
+              ftxui::color(ftxui::Color::DeepPink3)};
+  auto text{ftxui::vbox({youngpastry, in_, search}) | ftxui::center};
+  auto spacer{ftxui::emptyElement() |
+              ftxui::size(ftxui::HEIGHT, ftxui::EQUAL, SPACER_HEIGHT)};
+  auto layout{ftxui::vbox({spacer, text})};
+  return layout;
 }
 
 template <class... Ts> struct Overload : Ts... {
